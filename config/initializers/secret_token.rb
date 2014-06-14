65e7ae2463502b49d571cf5702bf39b8876eed7c
mix irving <whimful@gmail.com>
2014-06-14 17:08:11 +1200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Engagenz::Application.config.secret_token = 'b8b03afcfdabd202a1c27bb7e54e99af11b10628e87971b63d3b3f651a761cd7f742944ba8db71efcc03c6190356acac519ad204f1a3afb4ed979629f9ce0579'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Engagenz::Application.config.session_store :cookie_store, key: '_engagenz_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Engagenz::Application.config.session_store :active_record_store
