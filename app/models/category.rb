14ba86c70d75f573777564a23106a7d56d2edcc8
mix irving <whimful@gmail.com>
2014-06-14 22:32:46 +1200

    jesse here take this

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,5 @@ class Category < ActiveRecord::Base
   attr_accessible :name
 
   validate :name, presence: true
-
   has_and_belongs_to_many :providers
 end
--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,7 +1,7 @@
 class SearchService
   def self.find(region: nil, categories: nil)
     Provider.where(region_id: region).
-             joins(:categories).where(type_id: type)
+             joins(:categories).where(categories: type)
   end
 
 
