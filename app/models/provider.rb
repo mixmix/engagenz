47e6e5adeeaac7321a5b1c0e27f7a7807ec32280
Jesse Doud <jessedoud@gmail.com>
2014-06-15 10:35:23 +1200

    add active_admin

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,5 +1,5 @@
 class Provider < ActiveRecord::Base
-  attr_accessible :name, :description, :address, :phone_number, :email, :website
+  attr_accessible :name, :description, :address, :phone_number, :email, :website, :region_id, :category_ids
 
   belongs_to :region
   has_and_belongs_to_many :categories
